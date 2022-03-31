2e0ae674cee29b8a682aef504a693be3a05d01cb
FishGoddess <1149062639@qq.com>
2022-03-31 22:00:29 +0800

    调整代码

--- internal/reflect.go
+++ internal/reflect.go
@@ -9,29 +9,48 @@ const (
 	tagKey = "props"
 )
 
+// getTag gets tag from field with tagKey.
+func getTag(field *reflect.StructField) (string, bool) {
+	tag, ok := field.Tag.Lookup(tagKey)
+	if !ok {
+		tag = field.Name
+	}
+
+	if tag == "-" {
+		return "", false
+	}
+
+	return tag, true
+}
+
 // mapToStruct maps m to v.
 func mapToStruct(entries map[string][]string, v interface{}) error {
+	val := reflect.ValueOf(v)
+	if val.Kind() != reflect.Struct {
+		return fmt.Errorf("props: the kind of v is %s, not struct", val.Kind())
+	}
+
 	return nil // TODO
 }
 
 // mapFromStruct maps v to m.
 func mapFromStruct(v interface{}) (entries map[string][]string, err error) {
-	t := reflect.TypeOf(v)
-	if t.Kind() != reflect.Struct {
-		return nil, fmt.Errorf("props: the kind of v is %s, not struct", t.Kind())
+	val := reflect.ValueOf(v)
+	if val.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("props: the kind of v is %s, not struct", val.Kind())
 	}
 
 	entries = make(map[string][]string, 16)
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		tag, ok := field.Tag.Lookup(tagKey)
-		if !ok {
-			tag = field.Name
-		}
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		//tag := getTag(&field)
 
-		fieldType := field.Type
-		fieldType.Kind() // TODO
-		_ = tag
+		switch field.Kind() {
+		case reflect.Struct:
+		case reflect.Array:
+		case reflect.Slice:
+		case reflect.Map:
+		}
 	}
 
 	return entries, nil
