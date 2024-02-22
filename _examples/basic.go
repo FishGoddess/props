be759a3c41d8b34a305b03452a1ab75b27262b88
FishGoddess <1149062639@qq.com>
2024-02-22 20:34:49 +0800

    完善 store 测试

--- _examples/basic.go
+++ _examples/basic.go
@@ -4,6 +4,34 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/FishGoddess/props"
+)
+
+func main() {
+	// Load properties from a file.
+	properties, err := props.LoadFile("load.properties")
+	if err != nil {
+		panic(err)
+	}
+
+	// Get one key from properties and covert it into a integer.
+	fmt.Println(properties.Get("key1"))
+	fmt.Println(properties.Get("key1").Int())
+
+	// List all entries in properties.
+	// Returns false if you want to continue listing.
+	properties.List(func(key string, value props.Value) (finished bool) {
+		fmt.Println(key, value)
+		return false
+	})
+
+	// Use StoreFile to store a props to a file.
+	// What's more, we provide StoreMap and StoreWriter to store a props to map and writer.
+	err = props.StoreFile(properties, "store.properties")
+	if err != nil {
+		panic(err)
+	}
 }
